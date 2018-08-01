024e11e3a492e761366b7f733be14cb00e81e28b
weilin <1278813338@qq.com>
2018-08-01 19:05:36 +0800

    新版

--- juanpi/config/index.js
+++ juanpi/config/index.js
@@ -10,7 +10,13 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+        '/api':{
+            target:'https://webservice.juanpi.com',
+            host:'webservice.juanpi.com',
+            changeOrigin:true,
+        }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
